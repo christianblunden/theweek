2d18faed5b09e38c781418eecf71868ecbfffb45
Christian Blunden <christian.blunden@gmail.com>
2011-04-21 18:02:04 +0100

    initial prototype checkin

---
+++ Classes/TheWeekAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  TheWeekAppDelegate.h
+//  TheWeek
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface TheWeekAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
